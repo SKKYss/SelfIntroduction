2d0a18df98e9eb2e066c6d0a7490dccf8777bd49
SKKYss <1482273792@qq.com>
2025-04-09 16:33:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+//图片轮播1
+let oMain = document.querySelector("#main");
+let aImg = document.querySelector("#main div");
+let speed1 = -8;
+oMain.innerHTML += oMain.innerHTML;
+let move = () => {
+  if (oMain.offsetLeft < -oMain.offsetWidth / 2) {
+    oMain.style.left = "0";
+  }
+  if (oMain.offsetLeft > 0) {
+    oMain.style.left -= oMain.offsetLeft / 2 + "px";
+  }
+  oMain.style.left = oMain.offsetLeft + speed1 + "px";
+};
+let timer = setInterval(move, 30);
+
+//图片轮播2
+let oMain2 = document.querySelector("#main2");
+let oMask = document.querySelector("#mask");
+let aImg2 = document.querySelectorAll("#main2 div");
+let aButton = document.querySelectorAll("span");
+let speed2 = -15;
+oMain2.innerHTML += oMain2.innerHTML;
+let move2 = () => {
+  if (oMain2.offsetLeft < -oMain2.offsetWidth / 2) {
+    oMain2.style.left = "0";
+  }
+  if (oMain2.offsetLeft > 0) {
+    oMain2.style.left = -oMain2.offsetWidth / 2 + "px";
+  }
+  oMain2.style.left = oMain2.offsetLeft + speed2 + "px";
+};
+let timer2 = setInterval(move2, 30);
+oMask.addEventListener("mouseover", () => {
+  clearInterval(timer2);
+});
+oMask.addEventListener("mouseout", () => {
+  timer2 = setInterval(move2, 30);
+});
+aButton[0].onclick = () => {
+  speed2 = -15;
+};
+aButton[1].onclick = () => {
+  speed2 = 15;
+};
+// 处理滚动事件
+let lastScrollTop = 0; // 记录上一次滚动的位置
+window.addEventListener("scroll", function () {
+  const sections = document.querySelectorAll("section");
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+      if (scrollTop > lastScrollTop) {
+        // 向下滚动
+        section.classList.add("visible-down");
+        section.classList.remove("visible-up");
+      } else {
+        // 向上滚动
+        section.classList.add("visible-down");
+        section.classList.remove("visible-up");
+      }
+    } else {
+      section.classList.remove("visible-down", "visible-up");
+    }
+  });
+
+  lastScrollTop = scrollTop; // 更新上一次滚动的位置
+});
+
+// 页面加载时显示所有 section
+window.addEventListener("load", function () {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.classList.add("visible-down");
+  });
+});
